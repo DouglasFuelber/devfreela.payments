e637bd25af4ec5ad4bb7ef6917251bd5b83374f7
Douglas Fuelber <contato@douglasfuelber.com>
2021-10-18 23:05:11 -0300

    Created project

---
+++ DevFreela.Payments.API/DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Payments.API.Models;
+using System.Threading.Tasks;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> Process(PaymentInfoInputModel payment)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
